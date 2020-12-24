6eb6968a0db1e6bbd9c88e4a0ea62e50eb6759e4
Dmitriy Marinyuk <mdv.devops@gmail.com>
2020-12-25 01:25:16 +0400

    Ver 1.0

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "default" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["5.196.220.246/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
