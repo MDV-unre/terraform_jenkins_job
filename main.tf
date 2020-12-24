dde0c2cbe92b9ea93a8315279fdafa76d360c26d
Dmitriy Marinyuk <mdv.devops@gmail.com>
2020-12-25 02:07:29 +0400

    latest

--- main.tf
+++ main.tf
@@ -45,4 +45,11 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
