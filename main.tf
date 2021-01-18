0dc6f2ab246261488b6280045171ef973eb9050d
mdv-devops <73440665+mdv-devops@users.noreply.github.com>
2021-01-18 14:02:35 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "mdv-terraform-bucket"
+    bucket = "mdv-terraform-state"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
